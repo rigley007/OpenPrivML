95c6c21d6e29e20438e95f5ec8f54660c3e0da7c
Timothy Gonzales <edward753martinez@hotmail.com>
2025-01-16 19:53:02 -0300

    Update adv_image.py

--- adv_image.py
+++ adv_image.py
@@ -12,9 +12,9 @@ adv_img_path = cfg.adv_img_path
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv') != -1: # If it's a convolutional layer
         nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
+    elif classname.find('BatchNorm') != -1: # If it's a BatchNorm layer
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
